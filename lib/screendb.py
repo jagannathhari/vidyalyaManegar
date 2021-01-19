1dbac9c6c928d9e054d101e699cf871b6f775634
Jagannath Hari <77583902+jagannathhari@users.noreply.github.com>
2021-01-19 22:57:38 +0530

    Update screendb.py

--- lib/screendb.py
+++ lib/screendb.py
@@ -1,19 +1,12 @@
-import os
-
-from kivy.config import Config
-os.environ["KIVY_NO_FILELOG"] = "1"
-#os.environ["KIVY_NO_CONSOLELOG"] = "0"
-os.environ['KIVY_HOME'] = os.getcwd()
-Config.set('input', 'mouse', 'mouse,disable_multitouch')
-Config.set('graphics', 'resizable', True)
-Config.write()
-
-from kivy.core.window import Window
+import calendar
+from plyer import filechooser
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.tab import MDTabsBase
 from kivy.uix.floatlayout import FloatLayout
+from datetime import datetime
 from kivy.app import App
-
+from kivy.uix.screenmanager import SlideTransition
+from kivymd.uix.picker import MDDatePicker
 from lib import database
 from lib.dialog import Dialog
 from lib.setting import Setting
@@ -30,7 +23,7 @@ engine = database.db(backend, dbname=dbname,
                      username=username, password=password)
 
 conn = database.create(engine=engine)
-trans = conn.begin()
+conn.begin()
 dlg = Dialog()
 
 class ScreenDb(MDScreen):
@@ -50,7 +43,6 @@ class ScreenDb(MDScreen):
     def get_text(self):
         kwargs = {"addmission_no": self.ids.txt_ano.text,
                   "connection": conn,
-                  "trans": trans,
                   "name": self.ids.txt_name.text,
                   "father_name": self.ids.txt_fname.text,
                   "mother_name": self.ids.txt_mname.text,
@@ -83,7 +75,7 @@ class ScreenDb(MDScreen):
         self.btn_clear_click()
 
     def btn_Find_click(self):
-        data = database.get_detail(conn, trans,
+        data = database.get_detail(conn,
                                    self.ids.txt_ano.text)
         colmns = [i for i in range(8)] + [16]
 
@@ -116,7 +108,7 @@ class ScreenDb(MDScreen):
             dlg.title = "Enter Addmission Number"
             dlg.open()
             return
-        data = database.find_marks(conn, trans, ano)
+        data = database.find_marks(conn, ano)
         if not data:
             dlg.text = "No data found"
             dlg.title = "Not found!"
@@ -133,9 +125,9 @@ class ScreenDb(MDScreen):
             dlg.title = "Enter Addmission Number"
             dlg.open()
             return
-        data = database.find_marks(conn, trans, ano)
+        data = database.find_marks(conn, ano)
         if data:
-            database.add_marks(conn, trans, ano, marks)
+            database.add_marks(conn, ano, marks)
         else:
             dlg.text = "Error"
             dlg.title = "No data found!"
@@ -143,7 +135,7 @@ class ScreenDb(MDScreen):
             return
 
     def btn_Remove_click(self):
-        data = database.get_detail(conn, trans,
+        data = database.get_detail(conn,
                                    self.ids.txt_ano.text)
         ano = self.ids.txt_ano.text
         if not ano:
@@ -152,7 +144,7 @@ class ScreenDb(MDScreen):
             dlg.open()
             return
         if data:
-            database.delete(conn, trans, self.ids.txt_ano.text)
+            database.delete(conn, self.ids.txt_ano.text)
         else:
             dlg.text = "Error"
             dlg.title = "No data found!"
@@ -161,29 +153,56 @@ class ScreenDb(MDScreen):
 
     def create_card(self):
 
-        data = database.find_marks(conn, trans, self.ids.txt_an.text)
+        data = database.find_marks(conn, self.ids.txt_an.text)
         if not data:
             dlg.text = "No data found"
             dlg.title = "Not found!"
             dlg.open()
             return
         else:
-            total = 0
-            root = App.get_running_app().root  #
-            for i, j in zip(data.split(), range(7)):
-                total += float(i)
-                root.ids.scr.get_screen(
-                    'screencard').ids[f"txt_{j}"].text += (" " * 100) + i
+            total = sum(map(float, data.split()))
+            detail = database.get_detail(conn, self.ids.txt_an.text)
+            indent = ' ' * 10
+            student_detail = f'''
+            Name:-        {detail[0]}
+            Father's Name:-        {detail[1]}{indent}Mother's Name:- {detail[2]}
+            Class:-        {detail[5]}{indent}Sec:-        {detail[6]}
 
-            root.ids.scr.get_screen(
-                'screencard').ids.txt_total.text = "Total " + str(total)
-            root.ids.scr.get_screen(
-                'screencard').ids.txt_percent.text \
-                = "Percent " + str(total / 8)
+            '''
+            root = App.get_running_app().root
+            screen_card = root.ids.scr.get_screen('screencard')
+            root.ids
+            subjects = ['English', 'Math', 'Science', 'Computer',
+                        'Hindi', 'Social', 'Music', 'Art']
+            screen_card.ids.lbl_detail.text = student_detail
+            screen_card.ids.txt_sub.text = \
+                "\n".join(subjects + [f"Percentage {total/8}"])
+
+            screen_card.ids.txt_marks.text = \
+                "\n".join(data.split() + [f"Total {total}"])
+
             App.get_running_app().root.ids.scr.current = 'screencard'
 
     def btn_setting_click(self):
         App.get_running_app().root.ids.scr.current = 'settings'
+
+    def format_date(self, date=""):
+        if date == "":
+            date = datetime.now().date()
+        months = calendar.month_name
+        month = months[date.month]
+        year = date.year
+        day = date.day
+        final = f"{day} {month} {year}"
+        return final
+
+    def set_date(self, date):
+        date = self.format_date(date)
+        self.ids.item_month.set_item(date)
+
+    def select_date(self):
+        date_dialog = MDDatePicker(callback=self.set_date)
+        date_dialog.open()
 
 
 class Tab(FloatLayout, MDTabsBase):
@@ -191,4 +210,13 @@ class Tab(FloatLayout, MDTabsBase):
 
 
 class ScreenCard(MDScreen):
-    pass
+    def btn_save_click(self):
+        result = filechooser.save_file()
+        if result:
+            self.ids.card_report.export_to_png(result[0] + ".png")
+
+    def btn_back_click(self):
+        root = App.get_running_app().root.ids.scr
+        root.transition = SlideTransition(direction="right")
+        root.current = "screendb"
+        root.transition = SlideTransition()
